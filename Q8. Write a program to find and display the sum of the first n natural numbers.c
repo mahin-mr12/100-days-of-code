aa4ada540f0a3a3e4009023a1784c25da3de2e51
mahin-mr12 <mahin12mraj@gmail.com>
2025-10-14 13:33:42 +0530

    Create Q8. Write a program to find and display the sum of the first n natural numbers.c

---
+++ Q8. Write a program to find and display the sum of the first n natural numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter a positive integer (n): ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Please enter a positive number.\n");
+    } else {
+        for (i = 1; i <= n; ++i) {
+            sum += i;
+        }
+
+        printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+    }
+
+    return 0;
+}
